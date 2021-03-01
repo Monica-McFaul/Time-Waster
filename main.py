7a4427518dafcc8a8d5802554eaf841344a9bf29
MonicaMcFaul <monica.mcfaul@ocsbstudent.ca>
2021-03-01 14:38:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Welcome to The Critical Roll! You are the finest fighter in the land, jaded by years of experience you find yourself wandering through the woods when, upon this fine morning, you see a wolf digging through your belongings!")
+
+player = (input("How do you want to do this: Attack? or leave? "))
+
+if player == "Attack":
+  print ("Roll an Attack!")
+
+if player == "Leave":
+  print ("The wolf stole two days of food!")
+
+player = (input("Roll an Attack "))
+
+if player == "Roll an Attack":
+  print ("You rolled a 14, with your modifier of 2 and proficiency of 2, that's an 18 to hit. It hits! Roll damage!")
+
